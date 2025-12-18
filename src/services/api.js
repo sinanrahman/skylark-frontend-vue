5994dd1b8e0e6be12a7e763993a59f43b30801ee
sinansrk <sinansrk966@gmail.com>
2025-12-18 17:59:57 +0530

    api route changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "http://webliq.ap-1.evennode.com/",
     withCredentials: true
 })
 
