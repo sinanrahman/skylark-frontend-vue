07c2e22780c237823c86433b1dd40cef2ec38a21
sinansrk <sinansrk966@gmail.com>
2025-12-18 20:51:30 +0530

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://skylark-imji.onrender.com/api",
+    baseURL: "https://skylark-backend-4pfl.onrender.com/api",
     withCredentials: true
 })
 
